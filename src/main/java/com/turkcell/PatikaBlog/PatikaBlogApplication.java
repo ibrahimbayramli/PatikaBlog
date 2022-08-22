9c5084bae9a2c9fae6e04bc76c423234bbd54c5f
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-08-22 15:20:04 +0300

    PatikaBlog

---
+++ src/main/java/com/turkcell/PatikaBlog/PatikaBlogApplication.java
@@ -0,0 +1,14 @@
+package com.turkcell.PatikaBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PatikaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatikaBlogApplication.class, args);
+	}
+
+}
